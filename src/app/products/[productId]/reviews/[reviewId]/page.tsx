692b1cab5dab73400d434dcf437e6ea85bec5dc9
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-16 19:38:37 +0300

    not-found page and notFound func

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,12 @@
+import { notFound } from "next/navigation";
+
 interface ReviewProps {
   params: Promise<{ productId: string; reviewId: string }>;
 }
 
 export default async function Review({ params }: ReviewProps) {
   const { productId, reviewId } = await params;
+  if (parseInt(reviewId) > 1000) notFound();
   return (
     <h1>
       Review {reviewId} for Product {productId}
