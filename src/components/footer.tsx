a6f246675c9109c72da2b26290d6c885efde726e
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-17 12:07:02 +0300

    layout

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-400 p-5 text-center">
+      <h1>Footer</h1>
+    </footer>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-amber-400 p-5 text-center">
+      <h1>Header</h1>
+    </header>
+  );
+}
