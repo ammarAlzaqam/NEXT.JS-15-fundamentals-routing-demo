fad7093d0efe723bbe529be9dcae7b94e95e7b7d
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-22 05:58:16 +0300

    link component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Products() {
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Product Details</h1>
       <h2>
         <Link href={`products/1`}>Product 1</Link>
@@ -11,7 +12,7 @@ export default function Products() {
         <Link href={`products/2`}>Product 2</Link>
       </h2>
       <h2>
-        <Link href={`products/3`}>Product 3</Link>
+        <Link href={`products/3`} replace>Product 3</Link>
       </h2>
     </>
   );
