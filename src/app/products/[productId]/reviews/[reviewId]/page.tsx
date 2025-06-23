4fe26aac9848f4ad069afcf441305a5c02ebb394
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-23 05:44:29 +0300

    Error Handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -4,8 +4,12 @@ interface ReviewProps {
   params: Promise<{ productId: string; reviewId: string }>;
 }
 
+const getRandomInt = (count: number) => Math.floor(Math.random() * count);
+
 export default async function Review({ params }: ReviewProps) {
   const { productId, reviewId } = await params;
+  const random = getRandomInt(2);
+  if (random === 1) throw new Error("Error loading review");
   if (parseInt(reviewId) > 1000) {
     // noFound();
     redirect("/products");
