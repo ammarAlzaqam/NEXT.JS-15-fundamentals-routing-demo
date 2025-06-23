558761f6f932efed65ae2264559cf6230eb60ce3
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-23 06:29:00 +0300

    Recovering from Errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,30 @@
 "use client";
 
-export default function Error({ error }: { error: Error }) {
-  return <h1>{error.message}</h1>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  return (
+    <>
+      <h1>{error.message}</h1>
+      <button
+        className="bg-amber-300 p-1 rounded-md text-gray-500 cursor-pointer hover:bg-white hover:text-black transition"
+        onClick={() => {
+          startTransition(() => {
+            router.refresh();
+            reset();
+          });
+        }}
+      >
+        try again
+      </button>
+    </>
+  );
 }
