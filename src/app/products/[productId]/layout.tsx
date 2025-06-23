66f36d5c0544fd06d2d4eaf356bd6e3298821009
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-23 07:37:23 +0300

    Handling Errors in layouts

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from "react";
 
+const getRandomInt = (count: number) => Math.floor(Math.random() * count);
+
 export default function ProductDetailsLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const random = getRandomInt(2);
+  if (random === 1) throw new Error("Error loading product");
   return (
     <div>
       {children}
